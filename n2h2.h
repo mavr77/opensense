b2d56199c3f739accc581de38a6a92ac91eb144f
Yuriy Goroshanskiy <yuriy.goroshanskiy@gmail.com>
2018-02-01 18:16:59 +0200

    add description n2h2

---
+++ n2h2.h
@@ -0,0 +1,32 @@
+#define N2H2 1
+#define N2H2_HDR 10
+#define N2H2_ALIVE 515
+#define N2H2_ALIVE_RESP 770
+#define N2H2_ALIVE_SIZE 20
+#define N2H2_REQ 512
+#define N2H2_REQ_ACCEPT 2
+#define N2H2_REQ_DENY 258
+#define N2H2_REQ_SIZE 18
+
+struct n2h2_req {
+    uint16_t code;
+    uint32_t serial;
+    uint32_t srcip;
+    uint32_t dstip;
+    uint16_t urlsize;
+    uint16_t usrsize;
+    char url[URL_SIZE];
+} __attribute__((__packed__));
+
+struct n2h2_resp {
+    uint16_t code;
+    uint32_t serial;
+    uint16_t unknown;
+    uint16_t urlsize;
+    char url[URL_SIZE];
+} __attribute__((__packed__));
+
+extern void n2h2_alive(int fd, struct n2h2_req *n2h2_request);
+extern void n2h2_accept(int fd, struct n2h2_req *n2h2_request);
+extern void n2h2_deny(int fd, struct n2h2_req *n2h2_request, char *redirect_url);
+extern struct uf_request n2h2_validate(struct n2h2_req *n2h2_request, int msgsize);
