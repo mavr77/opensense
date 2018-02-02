76f51a22d1378a8a1de4d57aaa638bb9bec5b9d7
Aleks Tkachenko <ale.tkachenko@gmail.com>
2018-02-02 08:58:30 -0800

    Added deny req logic

--- n2h2.c
+++ n2h2.c
@@ -26,5 +26,20 @@ void n2h2_accept(int fd, struct n2h2_req *n2h2_request)
 
 void n2h2_deny(int fd, struct n2h2_req *n2h2_request, char *redirect_url)
 {
-
+  int urlsize = 0;
+  int i; // counter for url bytes copy
+  struct n2h2_resp n2h2_resp_deny;
+  n2h2_resp_deny.code = htons(N2H2_REQ_DENY);
+  n2h2_resp_deny.serial = n2h2_request->serial;
+  n2h2_resp_deny.unknown = htons(0);
+  if(redirect_url != NULL){
+    urlsize = strlen(redirect_url) + 1;
+    if(urlsize < URL_SIZE){
+      n2h2_resp_deny.urlsize = htons(urlsize);
+      for(i=0;i<urlsize;i++){
+        n2h2_resp_deny.url[i] = redirect_url[i];
+      }
+    } // no nwed to check for urlsize > URL_SIZE cause it needs to send response anyways and it will fill it in with zeros which is expected reply
+  }
+  printf("%d\n", (int)send(fd, &n2h2_resp_deny, N2H2_HDR + urlsize, 0));
 }
